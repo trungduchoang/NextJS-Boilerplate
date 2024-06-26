bb01564116a279f4e82b578e8a70865df24be881
ThoaiVV <thoaivv@aladintech.co>
2024-06-26 11:19:53 +0700

    Update Layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,5 @@
-import { ReactNode, useState } from "react";
-import Sidebar from "./Sidebar";
-import Header from "./Header";
+import { ReactNode } from "react";
 import classes from "./GlobalLayout.module.scss";
-import { HEADER_HEIGHT } from "./variables";
 
 type TProps = {
   children: ReactNode;
@@ -11,23 +8,11 @@ type TProps = {
  * GlobalLayout
  */
 export default function GlobalLayout({ children }: TProps) {
-  const [sideBarVisible, setSideBarVisible] = useState(false);
-
   return (
     <div className={classes.root}>
-      <Header setSideBarVisible={setSideBarVisible} />
-      <div
-        className={classes.childrenWrapper}
-        style={{
-          paddingTop: HEADER_HEIGHT,
-        }}
-      >
+      <div className={classes.childrenWrapper}>
         <div className={classes.childrenInner}>{children}</div>
       </div>
-      <Sidebar
-        sideBarVisible={sideBarVisible}
-        setSideBarVisible={setSideBarVisible}
-      />
     </div>
   );
 }
